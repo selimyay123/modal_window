51b6fe9a4aada8468ccfe8fac7e83bde0890a797
Selim Yay <selimyay@192.168.1.3>
2025-10-31 02:05:09 -0700

    selected-the-necessary-elements

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+// select the element that we need
+// store the selections in variables
+// so we can use them over and over again
+
+/* whenever we use querySelector with a selector, which actually matches multiple elements, only the first one will get selected. use querySelectorAll instead */
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}
